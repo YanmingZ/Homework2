0cbc5307e16de6ecb376a57451dce0e95ddea7e0
YanmingZ <115550398+YanmingZ@users.noreply.github.com>
2023-02-12 20:47:58 +0100

    Add files via upload

---
+++ Masking_Bitwise_Operations.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.0;
+
+contract Masking_Bitwise_Operations{
+
+  function masking_bitwise_operations(uint8 input) pure public returns(uint[] memory) {
+
+    uint i = 0;
+    uint8 mask = 1;
+    uint[] memory output = new uint[](8);
+
+    for (i; i < 8; i++) {
+        output[i] = input & mask;
+        mask = mask << 1;
+    }
+
+   return output;
+  }
+}
